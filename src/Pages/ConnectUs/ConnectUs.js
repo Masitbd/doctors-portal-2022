6353bb8cff72c63e0fb866e230793564f11b62b5
Murad H Sarker <sarker24@gmail.com>
2022-05-12 12:36:29 +0200

    Fix contactus form

--- src/Pages/ConnectUs/ConnectUs.js
+++ src/Pages/ConnectUs/ConnectUs.js
@@ -2,18 +2,45 @@ import React from "react";
 
 const ConnectUs = () => {
   return (
-    <div class="hero bg-appointment-pattern">
-      <div class="hero-overlay bg-opacity-60"></div>
-      <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
-          <h3 class="mb-5 text-5xl font-bold">Contact Us</h3>
-          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-          <p class="mb-5">
+    <div className="hero bg-appointment-pattern my-6">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h3 className="mb-2 text-2xl font-bold">Contact Us</h3>
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <form>
+            <input
+              className="my-2 w-full"
+              type="email"
+              name=""
+              id=""
+              placeholder="Enter your email"
+            />
+
+            <input
+              className="w-full"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your message"
+            />
+            <br />
+            <textarea
+              className="my-2 w-full h-24"
+              placeholder="Enter your message"
+            ></textarea>
+            <br />
+            <input
+              className="btn btn-primary mt-0"
+              type="submit"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
     </div>
--- src/Pages/TestominialDetail/TestominialDetail.js
+++ src/Pages/TestominialDetail/TestominialDetail.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const TestominialDetail = ({ img }) => {
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
         <p>
           It is a long established fact that by the readable content of a lot
           layout. The point of using Lorem a more-or-less normal distribu to
